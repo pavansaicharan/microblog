7d9474359c1b1e314f70dcedb94113d8381f32ee
pavansaicharan <nayudu.0104@gmail.com>
2024-09-21 08:06:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask import Flask, render_template, request
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 load_dotenv()
 
 def create_app():
